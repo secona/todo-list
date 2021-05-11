24ae30c7dc69042cff43ae4a363a3cd11268f2f7
secona <77039267+secona@users.noreply.github.com>
2021-05-11 09:08:48 +0700

    create: user services

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,32 +1,23 @@
 import { RequestHandler } from 'express';
 import * as bcrypt from 'bcrypt';
 import User, { IUser } from '../models/user.model';
+import * as userServices from '../services/user.service';
 import { SALT_ROUNDS } from '../constants';
 
-// TODO: user service
 export const byId: RequestHandler = (req, res) => {
   const id = req.params.id;
-  User.findById(id, undefined, undefined, (error, data) => {
-    if (!data) {
-      return res.status(404).json({
-        error: { message: `User with id ${id} not found` },
-      });
-    }
-
-    if (error) {
-      return res.status(400).json({ error });
-    }
-
-    return res.status(200).json({ data });
-  });
+  userServices
+    .getById(id)
+    .then(({ code, ...json }) => res.status(code).json(json))
+    .catch(error => res.status(500).json({ error }));
 };
 
 export const create: RequestHandler = async (req, res) => {
-  const password = await bcrypt.hash(req.body.password, SALT_ROUNDS);
-  const newUser: IUser = { ...req.body, password };
-  User.create(newUser)
+  const user = await userServices.createUser(req.body);
+  user
+    .save()
     .then(data => res.status(201).json({ data }))
-    .catch(error => res.status(400).json({ error }));
+    .catch(error => res.status(500).json({ error }));
 };
 
 export const update: RequestHandler = async (req, res) => {
@@ -41,7 +32,7 @@ export const update: RequestHandler = async (req, res) => {
     }
 
     if (error) {
-      return res.status(400).json({ error });
+      return res.status(500).json({ error });
     }
 
     return res.status(200).json({
@@ -53,19 +44,8 @@ export const update: RequestHandler = async (req, res) => {
 
 export const remove: RequestHandler = (req, res) => {
   const id = req.params.id;
-  User.findByIdAndRemove(id, { new: true }, (error, data) => {
-    if (!data) {
-      return res.status(404).json({
-        error: { message: `User with id ${id} not found` },
-      });
-    }
-
-    if (error) {
-      return res.status(400).json({ error });
-    }
-
-    return res.status(200).json({
-      message: `Successfully removed user with id ${id}`,
-    });
-  });
+  userServices
+    .deleteUser(id)
+    .then(({ code, ...json }) => res.status(code).json(json))
+    .catch(error => res.status(500).json({ error }));
 };
---
+++ src/services/user.service.ts
@@ -0,0 +1,56 @@
+import { CallbackError, UpdateQuery } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import User, { IUserDoc, IUser } from '../models/user.model';
+import { SALT_ROUNDS } from '../constants';
+
+interface IResult {
+  code: number;
+  message?: string;
+  error?: CallbackError | { message: string };
+  data?: IUserDoc;
+}
+
+//TODO: service for updating user
+
+export function getById(id: any) {
+  return new Promise<IResult>((resolve, reject) => {
+    User.findById(id).exec((error, data) => {
+      if (!data) {
+        return resolve({
+          code: 404,
+          error: { message: `User with id ${id} not found` },
+        });
+      }
+
+      if (error) return reject(error);
+      return resolve({ code: 200, data });
+    });
+  });
+}
+
+export function deleteUser(id: any) {
+  return new Promise<IResult>((resolve, reject) => {
+    User.findByIdAndRemove(id).exec((error, data) => {
+      if (!data) {
+        return resolve({
+          code: 404,
+          error: { message: `User with id ${id} not found` },
+        });
+      }
+
+      if (error) return reject(error);
+      return resolve({
+        code: 200,
+        message: `User with id ${id} successfully deleted`,
+      });
+    });
+  });
+}
+
+/** run `new User()` with hashed password */
+export async function createUser(data: IUser) {
+  return bcrypt
+    .hash(data.password, SALT_ROUNDS)
+    .then(password => Promise.resolve(new User({ ...data, password } as IUser)))
+    .catch(error => Promise.reject(error));
+}
