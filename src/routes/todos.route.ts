e592e6937c43bd2553252b671bf9d0d890e3f3b1
secona <77039267+secona@users.noreply.github.com>
2021-05-21 09:17:51 +0700

    improve todos route
    GET /todos/:id/all -> /todos/:id
    POST /todos/:id/new -> /todos/:id
    
    move validateMongoId to `router.all()`
    add token authentication

--- src/routes/todos.route.ts
+++ src/routes/todos.route.ts
@@ -1,21 +1,20 @@
 import express from 'express';
+import authenticateToken from '../middlewares/authenticateToken';
 import validateMongoId from '../validators/mongoId.validator';
 import todoValidators from '../validators/todo.validator';
 import todoController from '../controllers/todo.controller';
 
 const router = express.Router();
 
-router.get('/:id/all', validateMongoId, todoController.all);
-router.post(
-  '/:id/new',
-  validateMongoId,
-  todoValidators.todoBody(),
-  todoController.new
-);
+router.all('/:id*', validateMongoId, authenticateToken);
+
+router
+  .route('/:id')
+  .get(todoController.all)
+  .post(todoValidators.todoBody(), todoController.new);
 
 router
   .route('/:id/:todoId')
-  .all(validateMongoId)
   .get(todoController.getById)
   .put(todoValidators.todoBody(true), todoController.updateById)
   .delete(todoController.deleteById);
